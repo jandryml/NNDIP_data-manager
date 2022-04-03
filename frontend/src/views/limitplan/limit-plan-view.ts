ed9053a09b73b64b95c697a29773d4b0164ce0a1
Jan Dryml <honza.dryml@icloud.com>
2022-04-03 22:11:14 +0200

    limit plan - design proposition

--- frontend/src/views/limitplan/limit-plan-view.ts
+++ frontend/src/views/limitplan/limit-plan-view.ts
@@ -1,11 +1,10 @@
 import { LitElement, html, css, customElement } from 'lit-element';
-import '@vaadin/form-layout/src/vaadin-form-layout.js';
+import '@vaadin/button/src/vaadin-button.js';
+import '@vaadin/text-field/src/vaadin-text-field.js';
 import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/horizontal-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/checkbox/src/vaadin-checkbox.js';
-import '@vaadin/text-field/src/vaadin-text-field.js';
 import '@vaadin/combo-box/src/vaadin-combo-box.js';
-import '@vaadin/button/src/vaadin-button.js';
 
 @customElement('limit-plan-view')
 export class LimitPlanView extends LitElement {
@@ -20,116 +19,66 @@ export class LimitPlanView extends LitElement {
 
   render() {
     return html`
-<vaadin-form-layout>
- <vaadin-vertical-layout theme="spacing" style="align-items: center; flex: 3;">
-  <vaadin-vertical-layout theme="spacing">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Max temperature config </label>
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;" theme="spacing-s">
+ <h2>Temperature config </h2>
+ <vaadin-horizontal-layout style="align-self: flex-start; flex-wrap: wrap;" theme="spacing-xl">
+  <vaadin-checkbox style="align-self: flex-end;" tabindex="" label="Enabled" type="checkbox" value="on"></vaadin-checkbox>
+  <vaadin-horizontal-layout style="flex-wrap: wrap; align-self: flex-end;" theme="spacing">
+   <vaadin-text-field type="text" tabindex="" label="Optimal value"></vaadin-text-field>
+   <vaadin-text-field type="text" tabindex="" label="Tolerance"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-vertical-layout style="align-self: flex-end; flex-wrap: wrap;">
+   <h4>Max threshold violation actions</h4>
+   <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+    <vaadin-combo-box tabindex=""></vaadin-combo-box>
+    <vaadin-combo-box tabindex=""></vaadin-combo-box>
    </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on">
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="25.5"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="23"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
   </vaadin-vertical-layout>
-  <vaadin-vertical-layout theme="spacing">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Min temperature config</label>
+  <vaadin-vertical-layout style="align-self: flex-end;">
+   <h4>Min threshold violation actions</h4>
+   <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+    <vaadin-combo-box tabindex=""></vaadin-combo-box>
+    <vaadin-combo-box tabindex=""></vaadin-combo-box>
    </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on" checked>
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="20.5"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="23"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
   </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+ <h2>Co2 config</h2>
+ <vaadin-vertical-layout style="align-self: flex-start; flex-wrap: wrap;">
+  <vaadin-horizontal-layout style="align-self: flex-start; flex-wrap: wrap;" theme="spacing-xl">
+   <vaadin-checkbox style="align-self: flex-end;" tabindex="" label="Enabled" type="checkbox" value="on"></vaadin-checkbox>
+   <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+    <vaadin-combo-box style="align-self: flex-end;" tabindex="" label="Sensor"></vaadin-combo-box>
+    <vaadin-text-field type="text" tabindex="" label="Optimal value" style="align-self: flex-end;"></vaadin-text-field>
+    <vaadin-text-field type="text" tabindex="" label="Tolerance" style="align-self: flex-end;"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+   <vaadin-vertical-layout style="align-self: flex-end; flex-wrap: wrap;">
+    <h4>Max threshold violation actions</h4>
+    <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+     <vaadin-combo-box tabindex=""></vaadin-combo-box>
+     <vaadin-combo-box tabindex=""></vaadin-combo-box>
+    </vaadin-horizontal-layout>
+   </vaadin-vertical-layout>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout style="align-self: flex-start; flex-wrap: wrap;" theme="spacing-xl">
+   <vaadin-checkbox style="align-self: flex-end;" tabindex="" label="Enabled" type="checkbox" value="on"></vaadin-checkbox>
+   <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+    <vaadin-combo-box style="align-self: flex-end;" tabindex="" label="Sensor"></vaadin-combo-box>
+    <vaadin-text-field type="text" tabindex="" label="Optimal value" style="align-self: flex-end;"></vaadin-text-field>
+    <vaadin-text-field type="text" tabindex="" label="Tolerance" style="align-self: flex-end;"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+   <vaadin-vertical-layout style="align-self: flex-end; flex-wrap: wrap;">
+    <h4>Max threshold violation actions</h4>
+    <vaadin-horizontal-layout style="flex-wrap: wrap;" theme="spacing">
+     <vaadin-combo-box tabindex=""></vaadin-combo-box>
+     <vaadin-combo-box tabindex=""></vaadin-combo-box>
+    </vaadin-horizontal-layout>
+   </vaadin-vertical-layout>
+  </vaadin-horizontal-layout>
  </vaadin-vertical-layout>
- <vaadin-vertical-layout theme="spacing" style="flex: 3; align-items: center;">
-  <vaadin-vertical-layout theme="spacing">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Max humidity config</label>
-   </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on" checked>
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="55"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="35"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
-  </vaadin-vertical-layout>
-  <vaadin-vertical-layout theme="spacing">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Min humidity config</label>
-   </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on">
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="25"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="35"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
-  </vaadin-vertical-layout>
- </vaadin-vertical-layout>
- <vaadin-vertical-layout theme="spacing" style="flex: 3; align-items: center;">
-  <vaadin-vertical-layout theme="spacing" style="align-items: flex-start;">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Max Co2 config</label>
-   </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on" checked>
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="900"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="600"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
-  </vaadin-vertical-layout>
-  <vaadin-vertical-layout theme="spacing" style="align-items: flex-start;">
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Min Co2 config</label>
-   </vaadin-horizontal-layout>
-   <vaadin-checkbox style="align-self: center;" tabindex="" type="checkbox" value="on">
-     Enable 
-   </vaadin-checkbox>
-   <vaadin-text-field label="Threshold" style="align-self: center;" type="text" tabindex="" value="200"></vaadin-text-field>
-   <vaadin-text-field label="Tolerance" style="align-self: center;" type="text" tabindex="" value="400"></vaadin-text-field>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-    <label>Action </label>
-   </vaadin-horizontal-layout>
-   <vaadin-combo-box style="align-self: center;" tabindex=""></vaadin-combo-box>
-   <vaadin-button style="align-self: center;">
-     Add action 
-   </vaadin-button>
-  </vaadin-vertical-layout>
- </vaadin-vertical-layout>
-</vaadin-form-layout>
+ <vaadin-button style="align-self: center; margin-top: var(--lumo-space-l);">
+  Add Co2 config 
+ </vaadin-button>
+</vaadin-vertical-layout>
 `;
   }
 
