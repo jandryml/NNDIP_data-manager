b46b1d58479f1169abe4d73aab53781137157463
Jan Dryml <honza.dryml@icloud.com>
2022-06-21 20:59:16 +0200

    db - flatten changelog

--- src/main/resources/db/changelog/004-update_priority_on_existing_plans.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset drymlj:4
-
-UPDATE plan SET priority = 50, plan_type = 'TIME_PLAN'  WHERE id = 16;
-UPDATE plan SET priority = 100, plan_type = 'MANUAL_PLAN' WHERE id = 17;
-UPDATE plan SET plan_type = 'LIMIT_PLAN' WHERE id = 18;
-UPDATE plan SET plan_type = 'LIMIT_PLAN' WHERE id = 19;
-UPDATE plan SET priority = 10, plan_type = 'LIMIT_PLAN' WHERE id = 20;
-
